83a1bb9ddfcedec5bfd9100275eef5fc40b21067
Astemir Eleev <astemireleev@gmail.com>
2017-10-19 15:54:57 +0300

    Implemented additional model-view-projection transformations and added time property

--- MetalParticleSystem/Library/Node.swift
+++ MetalParticleSystem/Library/Node.swift
@@ -15,11 +15,12 @@ class Node {
     
     // MARK: - Properties
     
-    let devide: MTLDevice
+    let device: MTLDevice
     let name: String
     var vertexCount: Int
     var vertexBuffer: MTLBuffer
-    
+    var time: CFTimeInterval = 0.0
+
     // MARK: - Transform properties
     
     var positionX: Float = 0.0
@@ -45,7 +46,7 @@ class Node {
         
         self.vertexBuffer = vertexBuffer
         self.name = name
-        self.devide = device
+        self.device = device
         vertexCount = vertices.count
     }
     
@@ -58,30 +59,50 @@ class Node {
     ///   - pipelineState: is an instance of MTLRenderPipelineState class
     ///   - drawable: is an instance of CAMetalDrawable class
     ///   - clearColor: is an optional instance of MTLClearColor 
-    func render(in commandQueue: MTLCommandQueue, pipelineState: MTLRenderPipelineState, drawable: CAMetalDrawable, clearColor: MTLClearColor?) {
+    func render(in commandQueue: MTLCommandQueue, pipelineState: MTLRenderPipelineState, drawable: CAMetalDrawable, parentModelViewMatrix: Matrix4, projectionMatrix: Matrix4, clearColor: MTLClearColor?) {
         
         let renderPassDescriptor = MTLRenderPassDescriptor()
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture
         renderPassDescriptor.colorAttachments[0].loadAction = .clear
-        renderPassDescriptor.colorAttachments[0].clearColor = clearColor ?? MTLClearColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+        renderPassDescriptor.colorAttachments[0].storeAction = .store
         
         let commandBuffer = commandQueue.makeCommandBuffer()
         
         let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderEncoder?.setCullMode(MTLCullMode.front)
         renderEncoder?.setRenderPipelineState(pipelineState)
         renderEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount)
+        
+        let nodeModelMatrix = self.modelMatrix()
+        nodeModelMatrix.multiply(left: parentModelViewMatrix)
+        
+
+        let uniformBuffer = device.makeBuffer(length: MemoryLayout<Float>.size * Matrix4.numberOfElements * 2, options: [])
+        let bufferPointer = uniformBuffer?.contents()
+        
+        memcpy(bufferPointer, nodeModelMatrix.raw, MemoryLayout<Float>.size * Matrix4.numberOfElements)
+        let accumulatedPointer: UnsafeMutableRawPointer = bufferPointer! + MemoryLayout<Float>.size * Matrix4.numberOfElements
+        memcpy(accumulatedPointer, projectionMatrix.raw, MemoryLayout<Float>.size * Matrix4.numberOfElements)
+        
+        renderEncoder?.setVertexBuffer(uniformBuffer, offset: 0, index: 1)
+        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount, instanceCount: vertexCount/3)
         renderEncoder?.endEncoding()
         
         commandBuffer?.present(drawable)
         commandBuffer?.commit()
     }
     
+    func updateWithDelta(delta: CFTimeInterval){
+        time += delta
+    }
+    
     func modelMatrix() -> Matrix4 {
+        
         let matrix = Matrix4()
-        matrix.translate(positionX, y: positionY, z: positionZ)
-        matrix.rotateAroundX(rotationX, y: rotationY, z: rotationZ)
-        matrix.scale(scale, y: scale, z: scale)
+        matrix.translate(x: positionX, y: positionY, z: positionZ)
+        matrix.rotateAround(x: rotationX, y: rotationY, z: rotationZ)
+        matrix.scale(x: scale, y: scale, z: scale)
         return matrix
     }
     
