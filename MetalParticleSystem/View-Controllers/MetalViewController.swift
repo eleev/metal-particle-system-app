7a7bc574a114bb7d7df7b159c9d469661c53bbe4
Astemir Eleev <astemireleev@gmail.com>
2017-10-19 16:57:24 +0300

    Implemented MetalViewControllerDelegate protocol and refactored MetalViewController class

--- MetalParticleSystem/View-Controllers/MetalViewController.swift
+++ MetalParticleSystem/View-Controllers/MetalViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 import Metal
 
-class ViewController: UIViewController {
+protocol MetalViewControllerDelegate : class{
+    func updateLogic(timeSinceLastUpdate: CFTimeInterval)
+    func renderObjects(drawable: CAMetalDrawable)
+}
 
-    var objectToDraw: Cube!
+class MetalViewController: UIViewController {
+
+    // MARK: - Properties
     
     var device: MTLDevice!
     var metalLayer: CAMetalLayer!
@@ -21,59 +26,54 @@ class ViewController: UIViewController {
     var projectionMatrix: Matrix4!
     var lastFrameTimestamp: CFTimeInterval = 0.0
     
+    // MARK: - Delegate property
+    
+    weak var metalViewControllerDelegate: MetalViewControllerDelegate?
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         device = MTLCreateSystemDefaultDevice()
         
-        let angle = Float(85.0).radians
-        let aspectRatio = Float(self.view.bounds.size.width / self.view.bounds.size.height)
-        let nearZ: Float = 0.01
-        let farZ: Float = 100.0
+        let fov = Float(85).radians
+        let aspectRation = Float(self.view.bounds.size.width / self.view.bounds.size.height)
+        let nearZ = Float(0.01)
+        let farZ = Float(100.0)
         
-        projectionMatrix = Matrix4.makePerspectiveView(angle: angle, aspectRatio: aspectRatio, nearZ: nearZ, farZ: farZ)
+        projectionMatrix = Matrix4.makePerspectiveView(angle: fov, aspectRatio: aspectRation, nearZ: nearZ, farZ: farZ)
         
         metalLayer = CAMetalLayer()
-        metalLayer.device = device           
+        metalLayer.device = device
         metalLayer.pixelFormat = .bgra8Unorm
         metalLayer.framebufferOnly = true
         metalLayer.frame = view.layer.frame
         view.layer.addSublayer(metalLayer)
         
-        // Geometry setup
-        objectToDraw = Cube(device: device)
+        let defaultLibrary = device.makeDefaultLibrary()
+        let fragmentProgram = defaultLibrary?.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary?.makeFunction(name: "basic_vertex")
         
-        // Shader loading
-        let defaultLibrary = device.makeDefaultLibrary()!
-        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
-        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
-        
-        // Pipeline descriptor setup
         let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
         pipelineStateDescriptor.vertexFunction = vertexProgram
         pipelineStateDescriptor.fragmentFunction = fragmentProgram
         pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
         
-        // Pipeline setup
         pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
         
         commandQueue = device.makeCommandQueue()
         
-        timer = CADisplayLink(target: self, selector: #selector(ViewController.newFrame(displayLink:)))
+        timer = CADisplayLink(target: self, selector: #selector(MetalViewController.newFrame(displayLink:)))
         timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
     }
 
     // MARK: - Runloop
     
     func render() {
-        
         guard let drawable = metalLayer?.nextDrawable() else { return }
-        let worldModelMatrix = Matrix4()
-        worldModelMatrix.translate(x: 0.0, y: 0.0, z: -7.0)
-        worldModelMatrix.rotateAround(x: Float(25).radians, y: 0.0, z: 0.0)
-        
-        objectToDraw.render(in: commandQueue, pipelineState: pipelineState, drawable: drawable, parentModelViewMatrix: worldModelMatrix, projectionMatrix: projectionMatrix ,clearColor: nil)
+        self.metalViewControllerDelegate?.renderObjects(drawable: drawable)
     }
     
     @objc func newFrame(displayLink: CADisplayLink){
@@ -81,7 +81,6 @@ class ViewController: UIViewController {
         if lastFrameTimestamp == 0.0 {
             lastFrameTimestamp = displayLink.timestamp
         }
-        
         let elapsed: CFTimeInterval = displayLink.timestamp - lastFrameTimestamp
         lastFrameTimestamp = displayLink.timestamp
         
@@ -89,9 +88,8 @@ class ViewController: UIViewController {
     }
     
     func gameloop(timeSinceLastUpdate: CFTimeInterval) {
-        
-        objectToDraw.updateWithDelta(delta: timeSinceLastUpdate)
-        
+    
+        self.metalViewControllerDelegate?.updateLogic(timeSinceLastUpdate: timeSinceLastUpdate)
         autoreleasepool {
             self.render()
         }
