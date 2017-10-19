a2b5a94c3e22798092c072333428d7bfd6d52120
Astemir Eleev <astemireleev@gmail.com>
2017-10-19 15:57:02 +0300

    Refactored setup code, render method and added time measurements logic

--- MetalParticleSystem/ViewController.swift
+++ MetalParticleSystem/ViewController.swift
@@ -11,62 +11,87 @@ import Metal
 
 class ViewController: UIViewController {
 
+    var objectToDraw: Cube!
+    
     var device: MTLDevice!
     var metalLayer: CAMetalLayer!
-    var vertexBuffer: MTLBuffer! = nil
     var pipelineState: MTLRenderPipelineState!
-    var timer: CADisplayLink!
     var commandQueue: MTLCommandQueue!
-    
-    var objectToDraw: Cube!
+    var timer: CADisplayLink!
+    var projectionMatrix: Matrix4!
+    var lastFrameTimestamp: CFTimeInterval = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // MLTDevice setup
         device = MTLCreateSystemDefaultDevice()
         
-        // CAMetalLayer setup
+        let angle = Float(85.0).radians
+        let aspectRatio = Float(self.view.bounds.size.width / self.view.bounds.size.height)
+        let nearZ: Float = 0.01
+        let farZ: Float = 100.0
+        
+        projectionMatrix = Matrix4.makePerspectiveView(angle: angle, aspectRatio: aspectRatio, nearZ: nearZ, farZ: farZ)
+        
         metalLayer = CAMetalLayer()
-        metalLayer.device = device
+        metalLayer.device = device           
         metalLayer.pixelFormat = .bgra8Unorm
         metalLayer.framebufferOnly = true
         metalLayer.frame = view.layer.frame
         view.layer.addSublayer(metalLayer)
         
-        // Vertext Buffer setup
+        // Geometry setup
         objectToDraw = Cube(device: device)
         
-        // Render pipeline setup
-        let defaultLibrary = device.makeDefaultLibrary()
-        let fragmentProgram = defaultLibrary?.makeFunction(name: "basic_fragment")
-        let vertexProgram = defaultLibrary?.makeFunction(name: "basic_vertex")
+        // Shader loading
+        let defaultLibrary = device.makeDefaultLibrary()!
+        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
         
+        // Pipeline descriptor setup
         let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
         pipelineStateDescriptor.vertexFunction = vertexProgram
         pipelineStateDescriptor.fragmentFunction = fragmentProgram
         pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
         
+        // Pipeline setup
         pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
         
-        // Dispaly Link setup
-        
-        timer = CADisplayLink(target: self, selector: #selector(ViewController.gameloop))
-        timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
-        
         commandQueue = device.makeCommandQueue()
+        
+        timer = CADisplayLink(target: self, selector: #selector(ViewController.newFrame(displayLink:)))
+        timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
     }
 
     // MARK: - Runloop
     
     func render() {
         
-        guard let drawable = metalLayer.nextDrawable() else { return }
-        objectToDraw.render(in: commandQueue, pipelineState: pipelineState, drawable: drawable, clearColor: nil)
+        guard let drawable = metalLayer?.nextDrawable() else { return }
+        let worldModelMatrix = Matrix4()
+        worldModelMatrix.translate(x: 0.0, y: 0.0, z: -7.0)
+        worldModelMatrix.rotateAround(x: Float(25).radians, y: 0.0, z: 0.0)
+        
+        objectToDraw.render(in: commandQueue, pipelineState: pipelineState, drawable: drawable, parentModelViewMatrix: worldModelMatrix, projectionMatrix: projectionMatrix ,clearColor: nil)
     }
     
-    @objc func gameloop() {
+    @objc func newFrame(displayLink: CADisplayLink){
+        
+        if lastFrameTimestamp == 0.0 {
+            lastFrameTimestamp = displayLink.timestamp
+        }
+        
+        let elapsed: CFTimeInterval = displayLink.timestamp - lastFrameTimestamp
+        lastFrameTimestamp = displayLink.timestamp
+        
+        gameloop(timeSinceLastUpdate: elapsed)
+    }
+    
+    func gameloop(timeSinceLastUpdate: CFTimeInterval) {
+        
+        objectToDraw.updateWithDelta(delta: timeSinceLastUpdate)
+        
         autoreleasepool {
             self.render()
         }
